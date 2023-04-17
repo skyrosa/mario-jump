e8a68d17a01f43ffaf65424030abec779ac020c3
Scarlet Rosa <likedyourbody@gmail.com>
2023-04-17 02:46:24 +0200

    High Score

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
     const tunel = document.querySelector(".tunel");
     const scoreCount = document.querySelector(".score");
     const restart = document.querySelector("#restart");
+    const highScoreList = document.querySelector(".high-score");
 
     let jumpInProgress = false;
     let score = 0;
     let passedTunel = false;
     let jumpSound = new Audio('./audio/jump-sound.mp3');
     let gameOverSound = new Audio('./audio/game-over-sound.mp3');
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    const highestScore = highScores.length > 0 ? Math.max(...highScores) : 0;
+highScoreList.textContent = "High Score: " + highestScore;
 
     const bgMusic = new Audio('./audio/backgroundsound.mp3');
     bgMusic.loop = true;
@@ -79,6 +83,12 @@
             bgMusic.play();
           });
 
+          if (score > highestScore) {
+            highScore = score;
+            localStorage.setItem("highScore", highScore);
+            highScoreList.textContent = "High Score: " + highScore;
+          }
+
         restart.style.display = "block";
 
       }
@@ -117,7 +127,7 @@
             }
           }else {
             passedTunel = false; 
-          }
+          } 
         }, 10);
     
     if (mario) {
